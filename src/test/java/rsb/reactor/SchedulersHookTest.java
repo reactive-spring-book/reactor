b4d0afd8bad95c66769d17ab109af5868d34bfb5
Josh Long <josh@joshlong.com>
2019-11-24 21:23:37 -0800

    up

--- src/test/java/rsb/reactor/SchedulersHookTest.java
+++ src/test/java/rsb/reactor/SchedulersHookTest.java
@@ -17,22 +17,18 @@ public class SchedulersHookTest {
 
 	@Test
 	public void onScheduleHook() {
-		var threads = new ConcurrentSkipListSet<String>();
 		var counter = new AtomicInteger();
 		Schedulers.onScheduleHook("my hook", runnable -> () -> {
 			var threadName = Thread.currentThread().getName();
-			threads.add(threadName);
 			counter.incrementAndGet();
 			log.info("before execution: " + threadName);
 			runnable.run();
 			log.info("after execution: " + threadName);
-
 		});
 		Flux<Integer> integerFlux = Flux.just(1, 2, 3).delayElements(Duration.ofMillis(1))
 				.subscribeOn(Schedulers.immediate());
 		StepVerifier.create(integerFlux).expectNext(1, 2, 3).verifyComplete();
 		Assert.assertEquals("count should be 3", 3, counter.get());
-		// Assert.assertEquals("threads should be 3", 3, threads.size());
 	}
 
 }
