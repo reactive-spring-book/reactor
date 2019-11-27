809017ac804b9e0cfced268170d8d738ad4a47fe
Josh Long <josh@joshlong.com>
2019-11-27 20:46:39 +0900

    blockhound

--- src/test/java/rsb/reactor/BlockhoundTest.java
+++ src/test/java/rsb/reactor/BlockhoundTest.java
@@ -2,12 +2,21 @@ package rsb.reactor;
 
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.integration.BlockHoundIntegration;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.Spliterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.StreamSupport;
+
+import static reactor.blockhound.BlockHound.builder;
 
 // NB: if you want to run this on Java 13 in your IDE, make sure to add
 // -XX:+AllowRedefinitionToAddDeleteMethods
@@ -17,17 +26,49 @@ import reactor.test.StepVerifier;
 @Log4j2
 public class BlockhoundTest {
 
+	private final static AtomicBoolean BLOCKHOUND = new AtomicBoolean();
+
 	@Before
-	public void install() {
-		BlockHound.install();
+	public void before() {
+
+		BLOCKHOUND.set(true);
+		var integrations = new ArrayList<BlockHoundIntegration>();
+		var services = ServiceLoader.load(BlockHoundIntegration.class);
+
+		services.forEach(integrations::add);
+
+		integrations.add(builder -> builder.blockingMethodCallback(blockingMethod -> {
+			if (BLOCKHOUND.get()) {
+				throw new BlockingCallError(blockingMethod.toString());
+			}
+		}));
+
+		BlockHound.install(integrations.toArray(new BlockHoundIntegration[0]));
+	}
+
+	private static class BlockingCallError extends Error {
+
+		BlockingCallError(String msg) {
+			super(msg);
+		}
+
+	}
+
+	@After
+	public void after() {
+		BLOCKHOUND.set(false);
 	}
 
 	@Test
 	public void notOk() {
 		StepVerifier//
 				.create(this.buildBlockingMono().subscribeOn(Schedulers.parallel())) //
-				.expectErrorMatches(e -> e instanceof Error
-						&& e.getMessage().contains("Blocking call!"))//
+				.expectErrorMatches(e -> e instanceof BlockingCallError/*
+																		 * &&
+																		 * e.getMessage().
+																		 * contains("Blocking call!"
+																		 * )
+																		 */)//
 				.verify();
 	}
 
