a9f00140aa6afecbc4e6306b6f012b948be9280a
Josh Long <josh@joshlong.com>
2019-11-27 21:22:54 +0900

    polish

--- src/test/java/rsb/reactor/ControlFlowFirstTest.java
+++ src/test/java/rsb/reactor/ControlFlowFirstTest.java
@@ -13,9 +13,9 @@ public class ControlFlowFirstTest {
 	@Test
 	public void first() {
 		Flux<Integer> slow = Flux.just(1, 2, 3).delayElements(Duration.ofMillis(10));
-		Flux<Integer> fast = Flux.just(4, 5, 6).delayElements(Duration.ofMillis(2));
+		Flux<Integer> fast = Flux.just(4, 5, 6, 7).delayElements(Duration.ofMillis(2));
 		Flux<Integer> first = Flux.first(slow, fast);
-		StepVerifier.create(first).expectNext(4, 5, 6).verifyComplete();
+		StepVerifier.create(first).expectNext(4, 5, 6, 7).verifyComplete();
 	}
 
 }
