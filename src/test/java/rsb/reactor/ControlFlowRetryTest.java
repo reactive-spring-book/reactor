19c584bd1adce19f5c23f002e469674dfdbf009b
Josh Long <josh@joshlong.com>
2020-01-05 14:59:34 -0800

    reactor polish

--- src/test/java/rsb/reactor/ControlFlowRetryTest.java
+++ src/test/java/rsb/reactor/ControlFlowRetryTest.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.tools.agent.ReactorDebugAgent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -12,8 +13,8 @@ public class ControlFlowRetryTest {
 
 	@Test
 	public void retry() {
+
 		var errored = new AtomicBoolean();
-		// can we make retry() work? how do we demo it?
 		Flux<String> producer = Flux.create(sink -> {
 			if (!errored.get()) {
 				errored.set(true);
